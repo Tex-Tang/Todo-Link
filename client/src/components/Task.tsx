c0e71f5709943c762bb59ecbef25ef7fb23a560d
Tex Tang <tex_tang@outlook.com>
2022-01-06 00:41:33 +0800

    fix(client): click propagation

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -28,21 +28,21 @@ const Task: React.FC<TaskProps> = ({ task, onCheck, onClick, onDelete }) => {
       onClick={() => onClick(task)}
     >
       <motion.div className="flex">
-        {task.is_completed ? (
-          <motion.div
-            onClick={() => onCheck(task)}
-            className="w-4 h-4 mr-3 flex-shrink-0 flex-grow-0"
-            style={{ marginTop: ".2rem" }}
-          >
-            <FaCheckCircle className="text-white" style={{ fontSize: "1rem" }} />
-          </motion.div>
-        ) : (
-          <motion.div
-            onClick={() => onCheck(task)}
-            className="w-4 h-4 mr-3 rounded-full border-2 border-white flex-shrink-0 flex-grow-0"
-            style={{ marginTop: ".2rem" }}
-          ></motion.div>
-        )}
+        <motion.div
+          className="p-1 mr-2 relative z-10"
+          onClick={(e) => {
+            e.stopPropagation();
+            onCheck(task);
+          }}
+        >
+          {task.is_completed ? (
+            <motion.div className="w-4 h-4 flex-shrink-0 flex-grow-0">
+              <FaCheckCircle className="text-white" style={{ fontSize: "1rem" }} />
+            </motion.div>
+          ) : (
+            <motion.div className="w-4 h-4 rounded-full border-2 border-white flex-shrink-0 flex-grow-0"></motion.div>
+          )}
+        </motion.div>
 
         <motion.p>{task.title}</motion.p>
       </motion.div>
